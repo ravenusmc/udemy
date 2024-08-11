1e859f848335a79ded4163921654646961b5c1f2
ravenusmc <mcuddy77@gmail.com>
2024-08-11 11:50:31 -0400

    working on string methods for mysql

--- mysql/databases/string.sql
+++ mysql/databases/string.sql
@@ -1,3 +1,9 @@
 mysql -u root -p
 
 --Operations on strings 
+--Concat 
+SELECT CONCAT('h', 'e','l');
+
+SELECT CONCAT(author_fname, '!!!') FROM books;
+SELECT CONCAT(author_fname, ' ', author_lname) as 'Author Full Name' FROM books;
+SELECT CONCAT_WS(' ', author_fname, author_lname) as 'Author Full Name' FROM books;
