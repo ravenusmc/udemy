4080014765dbc92409c4e6a5d3826b3012060b4a
ravenusmc <mcuddy77@gmail.com>
2024-08-12 17:02:21 -0400

    worked on more string methods

--- mysql/databases/string.sql
+++ mysql/databases/string.sql
@@ -7,3 +7,7 @@ SELECT CONCAT('h', 'e','l');
 SELECT CONCAT(author_fname, '!!!') FROM books;
 SELECT CONCAT(author_fname, ' ', author_lname) as 'Author Full Name' FROM books;
 SELECT CONCAT_WS(' ', author_fname, author_lname) as 'Author Full Name' FROM books;
+
+--Working with Sub-string 
+SELECT SUBSTRING(title, 1, 15) FROM books; 
+SELECT SUBSTR(author_lname, 1,1) as 'first letter of last name', author_lname FROM books; 
