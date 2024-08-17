53c4f2b76bcaf1b0a773c93f7044312e6774b103
ravenusmc <mcuddy77@gmail.com>
2024-08-17 11:49:21 -0400

    finished lesson on substrings

--- mysql/databases/string.sql
+++ mysql/databases/string.sql
@@ -11,3 +11,9 @@ SELECT CONCAT_WS(' ', author_fname, author_lname) as 'Author Full Name' FROM boo
 --Working with Sub-string 
 SELECT SUBSTRING(title, 1, 15) FROM books; 
 SELECT SUBSTR(author_lname, 1,1) as 'first letter of last name', author_lname FROM books; 
+
+--Combing String functions 
+SELECT CONCAT(SUBSTR(title, 1, 10), '...') AS 'Short Title' FROM books; 
+SELECT 
+    CONCAT(SUBSTR(author_fname, 1,1), '.', SUBSTRING(author_lname,1,1), '.') AS 'Authors Initials' 
+FROM books; 
